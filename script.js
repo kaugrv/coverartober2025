fba3ddd34adee24c32fa8717359ebe108717ada9
Wendy GERVAIS <kau.grv@gmail.com>
2025-09-30 00:10:50 +0200

    add titles as tooltips

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let titles = [
+  ".*･｡ﾟ",
+  "1 - digital playground",
+  "2 - Câlin",
+  "3 - blue lightning",
+  "4 - SUNKISSED",
+  "5 - azerty vitamin ⁠◕",
+  "6 - Unis0n",
+  "7 - NeonBloodRace",
+  "8 - ALL ★",
+  "9 - Pommefreche",
+  "10 - Xoxo Baby Velour",
+  "11 - lunes furiosoooOO",
+  "12 - Purple Ash",
+  "13 - うずまき ⁠๑",
+  "14 - Aurore",
+  "15 - ENOCHIAN",
+  "16 - 1%",
+  "17 - kernel panic",
+  "18 - DCIM",
+  "19 - 999999kelvin",
+  "20 - baby steps",
+  "21 - SANG/BRULURE/SEL",
+  "22 - Angel Edge",
+  "23 - dress to depress",
+  "24 - 2DAY IS 4ME",
+  "25 - Chii Elixir",
+  "26 - 666gambling²",
+  "27 - RERERE",
+  "28 - где Надежда",
+  "29 - DSM-5 said ; ASD cues",
+  "30 - CRACKH34RT",
+  "31 - i.still.love.u",
+];
+
 function initAlbums() {
   // const date = new Date();
   // let day = date.getDate();
@@ -17,6 +52,7 @@ function initAlbums() {
     if (i <= day) img.src = `covers/${i}.jpg`;
     else img.src = "covers/blank.jpg";
     img.id = "album-" + i;
+    img.title = titles[i];
 
     a.appendChild(img);
 
