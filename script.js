99e2dcd00d83a2931e3fec8cddd7fe523e1c66d0
Wendy GERVAIS <kau.Grv@gmail.com>
2025-10-03 08:16:36 +0200

    day3

--- script.js
+++ script.js
@@ -37,19 +37,25 @@ function initAlbums() {
   // const date = new Date();
   // let day = date.getDate();
 
-  day = 2;
+  day = 3;
 
   for (let i = 0; i < 32; i++) {
     var div = document.createElement("div");
     div.classList.add("grid-item");
 
     var a = document.createElement("a");
-    if (i <= day) a.href = `covers/${i}.jpg`;
+    if (i <= day) {
+      if (i!=3) a.href = `covers/${i}.jpg`;
+      else a.href = `covers/${i}.gif`
+    }
     else a.href = "covers/blank.jpg";
     a.target = "_blank";
 
     var img = document.createElement("img");
-    if (i <= day) img.src = `covers/${i}.jpg`;
+    if (i <= day) {
+      if (i!=3) img.src = `covers/${i}.jpg`; 
+      else img.src = `covers/${i}.gif`
+    } 
     else img.src = "covers/blank.jpg";
     img.id = "album-" + i;
     img.title = titles[i];
