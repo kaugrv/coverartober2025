34b4bb677b8f8025be1cf11c77246195f636286e
Wendy GERVAIS <kau.Grv@gmail.com>
2025-10-04 21:59:54 +0200

    day4

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function initAlbums() {
   // const date = new Date();
   // let day = date.getDate();
 
-  day = 3;
+  day = 4;
 
   for (let i = 0; i < 32; i++) {
     var div = document.createElement("div");
